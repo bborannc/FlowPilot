db7df6a78c9f7723cd6ee1f133aba273dcbee464
Boran Çumaklar <borancumaklar@icloud.com>
2026-07-10 16:36:42 +0300

    feat: initial spring boot project structure

---
+++ src/main/java/com/enoca/flowpilot/FlowPilotApplication.java
@@ -0,0 +1,13 @@
+package com.enoca.flowpilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowPilotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowPilotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enoca/flowpilot/FlowPilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enoca.flowpilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowPilotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
